c30ce099a9c200b4a4c1d7d42a5586b616de0f36
Valeria Sainz <103615958+vsainz4@users.noreply.github.com>
2022-04-29 09:19:47 -0500

    Update and rename chap11.py to Sum of Number Extraction .py

--- Sum of Number Extraction .py
+++ Sum of Number Extraction .py
@@ -1,3 +1,5 @@
+#In this assignment you will read through and parse a file with text and numbers. 
+#You will extract all the numbers in the file and compute the sum of the numbers.
 #the sum should end in 850
 
 import re
